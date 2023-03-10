dc2e3636478f22aaac3798d19157b333cb4f6e57
ErTavo <66528756+ErTavo@users.noreply.github.com>
2023-03-10 06:53:55 -0600

    Add files via upload

--- src/app/producto/producto.component.ts
+++ src/app/producto/producto.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-producto',
   templateUrl: './producto.component.html',
   styleUrls: ['./producto.component.css']
+
+
 })
 export class ProductoComponent {
 
+
+  buscarEnGoogle(id: string) {
+
+      window.open(`https://www.google.com/search?q=${id}`);
+
+  }
+
 }
+
+
